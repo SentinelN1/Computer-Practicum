e5f7782be3f70976d0ea0e5131e9500bddcf1d91
SentinelN1 <egor.a.antonov@gmail.com>
2023-03-11 22:40:40 +0300

    Password Generator

--- Composite Pattern/PasswordGenerator.hpp
+++ Composite Pattern/PasswordGenerator.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include <algorithm>
 #include <string>
 #include <memory>
 #include <vector>
@@ -6,82 +7,100 @@
 class password_generator
 {
 public:
-    virtual std::string generate()
-    {
-    }
-
-    virtual std::string allowed_chars()
-    {
-    }
-
-    virtual size_t length()
-    {
-    }
-
-    virtual void add(std::unique_ptr<password_generator>)
-    {
-    }
-};
-
-class composite_password_generator : password_generator
-{
-private:
-    std::vector<std::unique_ptr<password_generator>> generators;
-
-public:
-    std::string generate()
-    {
-    }
-
-    void add(std::unique_ptr<password_generator>)
-    {
-    }
+    virtual std::string generate() = 0;
+    virtual std::string allowed_chars() = 0;
+    virtual size_t length() = 0;
+    virtual void add(password_generator *) {}
 };
 
 class basic_password_generator : public password_generator
 {
-private:
+protected:
     size_t len;
 
 public:
-    size_t length()
+    size_t length() { return len; }
+    basic_password_generator(const size_t &l = 0) { len = l; }
+    std::string generate()
     {
-        return len;
+        std::string password(len, ' ');
+        std::string allowedChars = allowed_chars();
+        size_t size = allowedChars.length();
+        for (size_t i = 0; i < len; i++)
+        {
+            password[i] = allowedChars[rand() % size];
+        }
+        return password;
     }
 };
 
 class digit_generator : public basic_password_generator
 {
 public:
-    std::string allowed_chars()
-    {
-        return "0123456789";
-    }
+    digit_generator(const size_t &l) { len = l; }
+    std::string allowed_chars() { return "0123456789"; }
 };
 
 class symbol_generator : public basic_password_generator
 {
 public:
-    std::string allowed_chars()
-    {
-        return "!@#$%&*?";
-    }
+    symbol_generator(const size_t &l) { len = l; }
+    std::string allowed_chars() { return "!@#$%&*?"; }
 };
 
 class upper_letter_generator : public basic_password_generator
 {
 public:
-    std::string allowed_chars()
-    {
-        return "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    }
+    upper_letter_generator(const size_t &l) { len = l; }
+    std::string allowed_chars() { return "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; }
 };
 
 class lower_letter_generator : public basic_password_generator
 {
 public:
+    lower_letter_generator(const size_t &l) { len = l; }
+    std::string allowed_chars() { return "abcdefghijklmnpqrstuvwxyz"; }
+};
+
+class composite_password_generator : password_generator
+{
+private:
+    std::vector<password_generator *> generators;
+
+public:
+    std::string generate()
+    {
+        std::string password = "";
+        for (password_generator *generator : generators)
+        {
+            password += generator->generate();
+        }
+        std::random_shuffle(password.begin(), password.end());
+        return password;
+    }
+
+    void add(password_generator *ptr)
+    {
+        generators.push_back(ptr);
+    }
+
+    size_t length()
+    {
+        size_t l = 0;
+        for (password_generator *generator : generators)
+        {
+            l += generator->length();
+        }
+        return l;
+    }
+
     std::string allowed_chars()
     {
-        return "abcdefghijklmnpqrstuvwxyz";
+        std::string allowedChars = "";
+        for (password_generator *generator : generators)
+        {
+            allowedChars += generator->allowed_chars();
+        }
+        return allowedChars;
     }
 };
--- Composite Pattern/Source.cpp
+++ Composite Pattern/Source.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 #include "PasswordGenerator.hpp"
+#include <ctime>
 
-int main() {
-    std::cout << "wtf";
+int main()
+{
+    std::srand(std::time(nullptr));
+
+    // digit_generator basic_password_generator(8);
+    // std::cout << basic_password_generator.generate() << "\n";
+
+    digit_generator digitGenerator(4);
+    std::cout << digitGenerator.generate() << "\n";
+
+    symbol_generator symbolGenerator(4);
+    std::cout << symbolGenerator.generate() << "\n";
+
+    upper_letter_generator upperLetterGenerator(4);
+    std::cout << upperLetterGenerator.generate() << "\n";
+
+    lower_letter_generator lowerLetterGenerator(4);
+    std::cout << lowerLetterGenerator.generate() << "\n";
+
+    composite_password_generator compositeGenerator;
+    compositeGenerator.add(&digitGenerator);
+    compositeGenerator.add(&symbolGenerator);
+    compositeGenerator.add(&upperLetterGenerator);
+    compositeGenerator.add(&lowerLetterGenerator);
+    std::cout << compositeGenerator.generate() << "\n";
+
     return 0;
 }
